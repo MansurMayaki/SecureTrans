614cbf1007375893b0c9202336f9e1f2040d4a9b
MansurMayaki <mansurmayaki@gmail.com>
2026-06-04 14:31:42 +0100

    Sprint 2

--- tests/test_rsa.py
+++ tests/test_rsa.py
@@ -1,1 +1,17 @@
 
+from crypto import RSAHandler
+
+rsa = RSAHandler()
+
+# Generate keys
+private_key, public_key = rsa.generate_key_pair()
+
+message = b"SecureTrans Test Message"
+
+# Encrypt
+encrypted = rsa.encrypt(public_key, message)
+print("Encrypted:", encrypted)
+
+# Decrypt
+decrypted = rsa.decrypt(private_key, encrypted)
+print("Decrypted:", decrypted)
